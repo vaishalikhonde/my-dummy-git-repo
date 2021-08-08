027af7eb5f0bb97e546b335934df9fd67211515e
Vaishali Khonde <vaishali.katole@gmail.com>
2021-08-08 16:51:48 +0530

    Made some corrections.

--- src/test/java/com/yantraQA/MyFirstGitClass.java
+++ src/test/java/com/yantraQA/MyFirstGitClass.java
@@ -2,16 +2,14 @@ package com.yantraQA;
 
 public class MyFirstGitClass {
 
-    public void MyFirstGitClass(){
-        void sum(int a , int b){
+    public void MyFirstGitClass(int a , int b){
             int sumVal = a+b;
             System.out.println("sum is :"+sumVal);
-        }
     }
 
     public  static void main(String args[]){
             MyFirstGitClass obj = new MyFirstGitClass();
-            obj.sum(10,20);
+            obj.MyFirstGitClass(10,20);
     }
 
 }
