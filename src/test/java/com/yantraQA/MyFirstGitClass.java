ae17f4fd7f0b0999be9681169b27016e8da84470
Vaishali Khonde <vaishali.katole@gmail.com>
2021-08-08 16:37:48 +0530

    Added changes in git.

--- src/test/java/com/yantraQA/MyFirstGitClass.java
+++ src/test/java/com/yantraQA/MyFirstGitClass.java
@@ -3,6 +3,9 @@ package com.yantraQA;
 public class MyFirstGitClass {
 
     public void MyFirstGitClass(){
-
+        void sum(int a , int b){
+            int sumVal = a+b;
+            System.out.println("sum is :"+sumVal);
+        }
     }
 }
